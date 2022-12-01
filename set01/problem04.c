9e5f4b9baf8eceea4fd240cbc2d2ee31b6011c8b
sahilsrksingh <117339264+srk142@users.noreply.github.com>
2022-12-01 09:42:07 +0000

    solved

---
+++ set01/problem04.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int sum (int *a, int *b);
+int main()
+{
+  int a, b;
+  printf("Enter two numbers: ");
+  scanf("%d %d", &a,&b);
+  int res = sum(&a,&b);
+  printf("Sum is %d + %d is %d",a,b,res);
+}
+int sum (int *a, int *b)
+{
+  return *a + *b;
+}
