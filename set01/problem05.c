ab6bada12bb288ec9bf7eb15f81ad66cecb1e6f5
sahilsrksingh <117339264+srk142@users.noreply.github.com>
2022-12-22 08:29:05 +0000

    solved

--- set01/problem05.c
+++ set01/problem05.c
@@ -2,3 +2,21 @@
 int input();
 void add(int a, int b, int c);
 void output(int a, int b, int c, int largest);
+
+int input()
+{
+  int n;
+  printf("enter a number\n");
+  scanf("%d", &n);
+  return n;
+}
+int find_largest(int a, int b, int c)
+{
+  return a>b?(a>c?a:c):(b>c?b:c);
+  
+}
+void output(int a, int b, int c, int largest)
+{
+  printf("enter a number \n");
+  
+}
